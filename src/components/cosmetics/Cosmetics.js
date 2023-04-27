32aae5b3df9b6edfa1d9b0dbdc78582372e228e2
Raju Islam <bdrajuislam246@gmail.com>
2023-04-27 17:37:25 +0600

    export & import

--- src/components/cosmetics/Cosmetics.js
+++ src/components/cosmetics/Cosmetics.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { add, multiplay } from '../../utilities/calculate';
+
 
 const Cosmetics = () => {
+    const first = 55;
+    const secound = 88;
+    const total = add(first,secound)
+   
     return (
+        
         <div>
-            <h1>Welcome to</h1>
+            <add></add>
+            <h1>Welcome to Cosmetics shop</h1>
+            <p>Total: {total}</p>
+            
         </div>
     );
 };
---
+++ src/utilities/calculate.js
@@ -0,0 +1,8 @@
+const add =(first,secound) =>{
+    return first +secound
+}
+
+const multiplay = (first, secound)=>{
+    return first * secound
+}
+export{add, multiplay}
